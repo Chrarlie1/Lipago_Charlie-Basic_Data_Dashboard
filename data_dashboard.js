3bf98741f614541232e6124e83e47b4648d9899d
Charlie <106720398+Chrarlie1@users.noreply.github.com>
2024-10-13 12:39:05 +0800

    Add files via upload

---
+++ data_dashboard.js
@@ -0,0 +1,7 @@
+
+const userTotal = document.getElementById("totalUsers").innerHTML;
+const saleTotal= document.getElementById("totalSales").innerHTML;
+
+  alertButton.addEventListener('click', function() {
+    alert('Total Users: '+ userTotal +', Total Sales: '+ saleTotal +'.');
+  }); 
